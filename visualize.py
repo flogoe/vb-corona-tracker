e1a5c40a67c505ce7298b96f4bd1de176d2531e8
Florian Goebel <florian.goebel@outlook.de>
2020-06-18 10:50:07 +0200

    Added visualize script.

--- visualize.py
+++ visualize.py
@@ -1,40 +1,24 @@
-import numpy as np
+import argparse
 import pandas as pd
-import seaborn as sns
 import matplotlib.pyplot as plt
-sns.set(style="white", rc={"axes.facecolor": (0, 0, 0, 0)})
+import re
 
-# Create the data
-rs = np.random.RandomState(1979)
-x = rs.randn(500)
-g = np.tile(list("ABCDEFGHIJ"), 50)
-df = pd.DataFrame(dict(x=x, g=g))
-m = df.g.map(ord)
-df["x"] += m
+def main():
+    parser = argparse.ArgumentParser()
+    parser.add_argument('logged_csv', type=str, help='CSV')
+    parser.add_argument('--style', nargs=1, default='-', help='Plot line style')
+    parser.add_argument('fields', metavar='field', nargs='*', help='REGEX')
+    args = parser.parse_args()
 
-# Initialize the FacetGrid object
-pal = sns.cubehelix_palette(10, rot=-.25, light=.7)
-g = sns.FacetGrid(df, row="g", hue="g", aspect=15, height=.5, palette=pal)
+    data = pd.read_csv(args.logged_csv, sep=';', index_col=0)
+    if args.fields:
+        regex_list = [re.compile(regex) for regex in args.fields]
+        field_names = list(data)
+        data_fields = list(filter(lambda field_name: any([True if regex.match(field_name) else False for regex in regex_list]), field_names))
+        data = data[data_fields]
+    fig, axes = plt.subplots()
+    plot = data.plot(ax=axes, kind='line', style=args.style, use_index=True)
+    plt.show()
 
-# Draw the densities in a few steps
-g.map(sns.kdeplot, "x", clip_on=False, shade=True, alpha=1, lw=1.5, bw=.2)
-g.map(sns.kdeplot, "x", clip_on=False, color="w", lw=2, bw=.2)
-g.map(plt.axhline, y=0, lw=2, clip_on=False)
-
-
-# Define and use a simple function to label the plot in axes coordinates
-def label(x, color, label):
-    ax = plt.gca()
-    ax.text(0, .2, label, fontweight="bold", color=color,
-            ha="left", va="center", transform=ax.transAxes)
-
-
-g.map(label, "x")
-
-# Set the subplots to overlap
-g.fig.subplots_adjust(hspace=-.25)
-
-# Remove axes details that don't play well with overlap
-g.set_titles("")
-g.set(yticks=[])
-g.despine(bottom=True, left=True)
+if __name__ == '__main__':
+    main()
